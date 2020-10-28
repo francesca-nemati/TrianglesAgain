d64e367c949439ee09dea12121d1779cd1cb1b6b
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2020-10-28 17:35:59 -0400

    added getPerimeter, fixed constructors and created testing file

---
+++ TestTriangle.java
@@ -0,0 +1,7 @@
+public class TestTriangle {
+  public static void main(String[] args) {
+    Triangle t1 = new Triangle(0,0,0,3,3,4);
+
+    System.out.println("Perimeter: " + t1.getPerimeter());
+  }
+}
--- Triangle.java
+++ Triangle.java
@@ -8,9 +8,31 @@ public class Triangle {
   }
 
   public Triangle(double x1, double y1, double x2, double y2, double x3, double y3) {
-    v1 = Point(x1,y1);
-    v2 = Point(x2,y2);
-    v3 = Point(x3,y3);
+    Point p1 = new Point(x1,y1);
+    Point p2 = new Point(x2,y2);
+    Point p3 = new Point(x3,y3);
+    v1 = p1;
+    v2 = p2;
+    v3 = p3;
   }
+
+  public double getPerimeter() {
+    double s1 = v1.distanceTo(v2);
+    double s2 = v2.distanceTo(v3);
+    double s3 = v3.distanceTo(v1);
+    return s1 + s2 + s3;
+  }
+
+  // public double getArea() {
+  //
+  // }
+  //
+  // public String classify() {
+  //
+  // }
+  //
+  // public String toString() {
+  //
+  // }
 
 }
