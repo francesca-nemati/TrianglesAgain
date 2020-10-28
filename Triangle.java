48ab5a10743ec225947d65fd6411efc09e1286ef
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2020-10-28 17:19:37 -0400

    added constructors

--- Triangle.java
+++ Triangle.java
@@ -1,6 +1,16 @@
 public class Triangle {
   private Point v1, v2, v3;
 
-  
+  public Triangle(Point a, Point b, Point c) {
+    v1 = a;
+    v2 = b;
+    v3 = c;
   }
+
+  public Triangle(double x1, double y1, double x2, double y2, double x3, double y3) {
+    v1 = Point(x1,y1);
+    v2 = Point(x2,y2);
+    v3 = Point(x3,y3);
+  }
+
 }
